2c0e65776b1519ba3fa9fdcd12cd6d5c344c4f9d
[adhish anand] <[adhishanand9@gmail.com]>
2019-03-09 09:44:55 +0530

    [Mouse Events using JavaScript]

---
+++ Mouse Events/Script.js
@@ -0,0 +1,6 @@
+var input=document.getElementById('inp');
+input.addEventListener("mousemove",function(event)
+{
+  inp.setAttribute("style","background-color: black;color:white");
+  console.log(1);
+});
