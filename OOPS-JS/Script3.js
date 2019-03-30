f345ef199c77a8f4e02159801445803928edf446
[adhish anand] <[adhishanand9@gmail.com]>
2019-03-30 11:48:04 +0530

    [Countdown Timer using JS and HTML]

--- OOPS-JS/Script3.js
+++ OOPS-JS/Script3.js
@@ -14,3 +14,16 @@ var person={
 person.changeName("Adhish","Anand");
 var name=person.returnName();
 console.log(name);
+var abc=person;
+abc.changeName("A","B");
+var name2=abc.returnName();
+console.log(name2);
+console.log(person.returnName());
+var s1={};
+for(var key in person){
+  s1[key]=person[key];
+}
+s1.changeName("Ka","SU");
+var name3=s1.returnName();
+console.log(name3);
+console.log(person.returnName());
